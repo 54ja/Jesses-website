ca7aab74dbf89fc6c1313ba6b435e965d064d9ea
54ja <ja54@mahurangi.school.nz>
2021-06-26 17:12:07 +1200

    part 4
    new colors linked photography page styled the nav.

---
+++ photography.php
@@ -0,0 +1,19 @@
+<?php include("head_nav.html"); ?>
+
+
+<div class="box main">
+	
+<h1>Photography</h1>
+
+</div>
+
+
+
+<div class="box side"> 
+
+Side
+
+</div>
+
+
+<?php include("footer.html"); ?>
