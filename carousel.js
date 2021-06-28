d6dd1dd687a0dfda02d0ae0d5cd96f54df3b5534
54ja <ja54@mahurangi.school.nz>
2021-06-28 14:45:08 +1200

    part 7
    added new java script to the carousel

--- carousel.js
+++ carousel.js
@@ -8,16 +8,32 @@ const dots = Array.from(dotsNavs.children);
 const slidesWidth = slides[0].getBoundingClientRect().width;
 
 
-console.log(slidesWidth);
-
 // arange the slides next to the aother one //
 
-slides[0].style.left = 0;
-slides[1].style.left = '100px';
+//slides[0].style.left = slidewidth * 0 + 'px';
+//slides[1].style.left = slidewidth * 1 + 'px';
+//slides[2].style,left = slidewidth * 2 + 'px';
 
+const setSlidePosition = (slide, index) => {
+	slide.style.left = slidesWidth * index + 'px';
+};
 
+slides.forEach(setSlidePosition);
 //when i click move slides to the left//
 
 //when i clickmove slides to the right//
 
+nextButton.addEventListener('click', e => {
+	const currentSlide = track.querySelector('.current-slide');
+	const nextSlide = currentSlide.nextElementSibling;
+	const amountToMove = nextSlide.style.left;
+
+	// move to the next slide
+	track.style.transform = 'translateX(-	' + amountToMove + ')';
+	currentSlide.classList.remove('current-slide');
+	nextSlide.classList.add('current-slide');
+
+
+})
 //when i click the nav idicators move to that slide//
+
