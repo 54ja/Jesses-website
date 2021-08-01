e01efa29a053a9be37cf6ca16625fae53d90c17b
54ja <ja54@mahurangi.school.nz>
2021-08-02 10:35:34 +1200

    Version 9
    createad a hole new website that uses bootstrap.

--- bootstrap website/contact.php
+++ bootstrap website/contact.php
@@ -1,13 +1,13 @@
 <?php include("head_nav.html"); ?>
         
-        <div class="box main">
+        <div class="box col-sm-8 main">
             
 
             <iframe class="contact" src="https://docs.google.com/forms/d/e/1FAIpQLSd9WDoa5Dezw6NXCGjy7YIZOD6hFxb-dpiyVo_9iBNRc8EUSw/viewform?embedded=true" height="673" ></iframe>
            
         </div>    <!-- / main -->
         
-        <div class="box side">
+        <div class="box col-sm-4 side">
             
             <h3>About Me</h3>
             
---
+++ bootstrap website/index.php
@@ -0,0 +1,17 @@
+<?php include("head_nav.html"); ?>
+
+
+      <div class="box main col-sm-8 ">
+      <h1>Art is cool</h1>
+      
+      <p>it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>  
+
+</div> <!-- / end of main  -->
+
+      <div class="box side col-sm-4"><p>it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.</p>  </div> <!-- / end of side  -->
+
+     
+      
+
+
+   <?php include("footer.html"); ?>
