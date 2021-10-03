6bc5ca5c97938d859be862d8edcccf4a43d59a67
54ja <78886190+54ja@users.noreply.github.com>
2021-10-03 15:24:50 +1300

    Version 13
    Added my photography page and added a carousel

--- photography.php
+++ photography.php
@@ -1,18 +1,100 @@
-<?php include("head_nav.html"); ?>
+<?php include("head_nav.html");?>
 
 
-<div class="box main">
-	
-	
+<div class="jumbotron text-center">
+
+</div>
+  
+<div class="about-section">
+  <h2><u>Our Art</u></h2>
+  <h5>Students at Mahurangi collge strive to be the best studensts they can and love to put themselves on display. This page highlights some of the art work that students have produced. Have a look around and if you have any questions please email me. ENJOY!
+</h5>
 </div>
 
 
+<div class="row">
+<div class="column">
+<div class="card">
+<img src="imagers/deer.jpg" alt="Photo of deer in forrest" style="width:100%">
+      <div class="container">
+        <h2>The deer in the forrest</h2>
+        <h4 class="title">By Bob Mullen</h4>
+        <h5>Bob Mullen, a student in year 10 took this photo when he was in the South Island on holiday. While traverling through the bush he came across it and managed to get a photo before it ran away.</h5>
+      </div>
+    </div>
+  </div>
 
-<div class="box side"> 
+  <div class="column">
+    <div class="card">
+      <img src="imagers/fruit.jpg" alt="Painting of fruit" style="width:100%">
+      <div class="container">
+        <h2>Summer's fruit</h2>
+        <h4 class="title">By Aimee Tomlinson</h4>
+        <h5>Aimee Tomlinson, a student in year 12 painted this art peice over the corse of a term for her level 2 art portfolio. The peice consists of diffrent fruits that are found in summer and brings out the vibarant colours that they have.</h5>
+      </div>
+    </div>
+  </div>
 
-Side
-
+  <div class="column">
+    <div class="card">
+      <img src="imagers/ladybug.jpg" alt="Macro picture of 2 lady bugs" style="width:100%">
+      <div class="container">
+        <h2>Lady bug love</h2>
+        <h4 class="title">By Eve Wade</h4>
+        <h5>Eve Wade, a student in year 9 took this photo over lock down in her back yard. Becasue of lock down everyone was forced to work from home. While trying to find somthing to captured she found 2 lady bugs and quickly got this incredibal photo. </h5>
+      </div>
+    </div>
+  </div>
 </div>
 
 
-<?php include("footer.html"); ?>
+<div class="row">
+<div class="column">
+<div class="card">
+<img src="imagers/flower.jpg" alt="Painting of pink flowers" style="width:100%">
+      <div class="container">
+        <h2>Pink flowers</h2>
+        <h4 class="title">By Robin Turner</h4>
+        <h5>Robin Turner, a student in year 13 has a large passion for drawing and doing paintings. For this years portfolio she came up with the idea to paint he grandmothers favorate flowers and then give it to her for her 94 birthday.</h5>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="imagers/highway.jpg" alt="Picture with low shutter speed" style="width:100%">
+      <div class="container">
+        <h2>Highway</h2>
+        <h4 class="title">By Rihanna Downs</h4>
+        <h5>Rihanna Downs, is a student in year 12 who has a love for photography and will take any opitunity to take a photo. This photo was taken in the busy streets of Auckland where Rihanna tested diffrent shutter speeds untill she got the perfict shot. </h5>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="imagers/moon.jpg" alt="Painting of the moon" style="width:100%">
+      <div class="container">
+        <h2>Moon and stars</h2>
+        <h4 class="title">Erik Harding</h4>
+        <h5>Erik Harding, is a student in year 11 who has passion for art. even since he was 8 he has been painting and has spent all of his spear time focusing his skills. This peice won the schools painting of the year award and even got second in nationals. </h5>
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+       
+
+
+<?php include("Carousel copy.html");?>
+  
+<?php include("footer.html");?>
+
+
+
+
+
+
